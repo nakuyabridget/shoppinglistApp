450fcefabef8b81399a456f4d73381caaf4e6d93
nakuyabridget <bnakuya7@gmail.com>
2018-07-05 11:20:47 +0300

    first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+
+@Component(
+    {
+        selector: 'app-header',
+        templateUrl: './header.component.html',
+    }
+)
+
+export class HeaderComponent {
+    // tslint:disable-next-line:no-trailing-whitespace
+    
+}
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+  new Recipe('A test Recipe', 'This is simply a test','https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29-1500x1125.jpg'),
+  new Recipe('A test Recipe', 'This is simply a test','https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29-1500x1125.jpg')
+  
+
+
+
+];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
