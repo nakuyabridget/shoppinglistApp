2e9052f09c9208767362dca46a37696b6f2d018a
nakuyabridget <bnakuya7@gmail.com>
2018-07-17 17:11:17 +0300

    using typescript to simplify constructors

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,15 +1,6 @@
 export class Ingredient {
-    public name: string;
-    public amount: number;
 
-    constructor(public name: string, public amount: number){
-        this.name = name;
-        this.amount = amount;
-
-    }
-
-
-
+    constructor(public name: string, public amount: number) { }
 
 
 }
