6291c82f8a81fe52c403d746d1298b39ccf1c2ac
nakuyabridget <bnakuya7@gmail.com>
2018-07-31 13:31:10 +0300

    local referencing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,7 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
   new Recipe('A test Recipe', 'This is simply a test',
   'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29-1500x1125.jpg'),
-  new Recipe('A test Recipe', 'This is simply a test',
+  new Recipe('Another test Recipe', 'This is simply a test',
   'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29-1500x1125.jpg')
 ];
 
