833d57cc0a8a70a3c694f4ecef39535f84c819c2
nakuyabridget <bnakuya7@gmail.com>
2018-08-08 09:47:52 +0300

    arrays are a referenced type in Angular

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {Recipe} from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,16 +9,12 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-  new Recipe('A test Recipe', 'This is simply a test',
-  'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29-1500x1125.jpg'),
-  new Recipe('Another test Recipe', 'This is simply a test',
-  'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29-1500x1125.jpg')
-];
+  recipes: Recipe[];
 
-  constructor() { }
+   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipe();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,6 @@
 import { Recipe } from './recipe.model';
 
-export class RecipesService {
+export class RecipeService {
    private recipes: Recipe[] = [
         new Recipe('A test Recipe', 'This is simply a test',
         'https://www.seriouseats.com/recipes/images/2016/12/20161201-crispy-roast-potatoes-29-1500x1125.jpg'),
